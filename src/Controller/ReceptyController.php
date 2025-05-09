4cd8f41ec1c0ec6ab7cb1e36af01aa88bca15264
Peter Gaľo <galopeter@outlook.com>
2025-05-09 09:01:29 +0200

    initial commit

---
+++ src/Controller/ReceptyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ReceptyController extends AbstractController
+{
+    #[Route('/recepty', name: 'app_recepty')]
+    public function index(): Response
+    {
+        return $this->render('recepty/index.html.twig', [
+            'controller_name' => 'ReceptyController',
+        ]);
+    }
+}
---
+++ src/Controller/RedirectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RedirectController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    #[Route('/{slug}', name: 'default_redirect', requirements: ['slug' => '.+'])]
+    public function redirectToMenu(): RedirectResponse
+    {
+        // Redirect all invalid or empty routes to the 'menu' route
+        return new RedirectResponse($this->generateUrl('app_menu'));
+    }
+}
